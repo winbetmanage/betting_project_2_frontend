b1cf2858aeac4298bccfc73989fc1315cf14dcf4
KidusThethird <kidus.z3rd@gmail.com>
2026-09-06 00:06:10 +0300

    config updated

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         source: "/api/v1/:path*",
         destination: `${API_BACKEND_URL}/api/v1/:path*`,
       },
+      {
+        source: "/health",
+        destination: `${API_BACKEND_URL}/health`,
+      },
     ];
   },
 };
